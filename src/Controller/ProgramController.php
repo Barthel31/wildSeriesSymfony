811f7d215fcd3f08fac6d6004888af8a0eb2e2a0
Barthel31 <sylvain.barthel@gmail.com>
2021-11-29 16:12:19 +0100

    add quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,16 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/program", name="program_")
+ * 
+ */
 Class ProgramController extends AbstractController
 {
     /**
-    * @Route("/program/", name="program_index")
+    * @Route("/", name="index")
+    *
+    * @return Response
     */
     public function index(): Response
     {
         return $this->render('/program/index.html.twig', [
         'website' => 'Wild Séries',
         ]);
+    }
+    
+    /**
+     * show
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="id")
+     * @param int
+     * @return string
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('/program/show.html.twig', ['id' => $id]);
     }
 }
